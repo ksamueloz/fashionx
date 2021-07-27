563a84fda88cc8787dbade9ce6f12acc9b23edad
Samuel Ortiz <kevinsamuel398@gmail.com>
2021-07-27 18:27:36 -0500

    CDN css, js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
-$(function() {
-    //LLamada a la función Tolltip y Popover
-    $("[data-toggle='tooltip']").tooltip();
-    $("[data-toggle='popover']").popover();
-    //Tiempo de 2 segundos para el carousel.
-    $('.carousel').carousel({
+(function() {
+    $('#carousel').carousel({
         interval: 2000
     });
 });
